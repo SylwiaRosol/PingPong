289cb5d5e966a41bba4420dc7ed0c55cae30465e
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2020-12-02 17:31:41 +0100

    Add paddle movement

---
+++ Unit1.h
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TShape *tlo;
+        TImage *ball;
+        TTimer *TimerBall;
+        TImage *paddle1;
+        TImage *paddle2;
+        TTimer *Paddle2Up;
+        TTimer *Paddle2Down;
+        TTimer *Paddle1Up;
+        TTimer *Paddle1Down;
+        void __fastcall TimerBallTimer(TObject *Sender);
+        void __fastcall Paddle2DownTimer(TObject *Sender);
+        void __fastcall Paddle2UpTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
